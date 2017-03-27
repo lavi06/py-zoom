352c5115631d4df1b8acb363c9bd6589eb33fd7e
lavi06 <hggoyal06@gmail.com>
2017-03-28 03:33:21 +0530

    newcommit

--- zoom.py
+++ zoom.py
@@ -4,11 +4,9 @@ import os
 from math import floor
 from math import ceil
 import matplotlib.pyplot as plt
-dir = os.path.dirname(__file__)
-filename = os.path.join(dir, 'rabbit.jpg')
 
-img=cv2.imread(filename , -1)
-def img_interp(img, K,piv_x,piv_y ):
+
+def img_zoom(img, K,piv_x,piv_y ):
 	
 	rows, cols , colors= img.shape
 	
@@ -35,30 +33,36 @@ def img_interp(img, K,piv_x,piv_y ):
 	new_img = np.ones((nrows, ncols, colors))
 	new_img=img[Ay:Dy, Ax:Bx]
 
-	enlarged_img = np.ones((rows, cols, colors))
+	zoomed_img = np.ones((rows, cols, colors))
 	
 	for i in range(rows-ceil(2*K+1)):
 		for j in range(cols-ceil(2*K+1)):
-			x_coord = j / K
-			y_coord = i / K
+			x_cord = j / K
+			y_cord = i / K
 			
-			xc = int(floor(x_coord+1))
-			xf = int(floor(x_coord))
-			yc = int(floor(y_coord+1))
-			yf = int(floor(y_coord))
+			b_x = int(floor(x_cord+1))
+			a_x = int(floor(x_cord))
+			d_y = int(floor(y_cord+1))
+			a_y = int(floor(y_cord))
 
-			W_xc = xc - x_coord
-			W_xf = x_coord - xf
-			W_yc = yc - y_coord
-			W_yf = y_coord - yf
+			L_b_x = b_x - x_cord
+			L_a_x = x_cord - a_x
+			L_d_y = d_y - y_cord
+			L_a_y = y_cord - a_y
 			
-			enlarged_img[i, j, :] =255-  np.around(W_xc * (W_yc * new_img[yf, xf, :] + W_yf * new_img[yc, xf, :]) + W_xf * (W_yc * new_img[yf, xc, :] + W_yf * new_img[yc, xc, :]), 0)
+			zoomed_img[i, j, :] =255-  np.around(L_b_x * (L_d_y * new_img[a_y, a_x, :] + L_a_y * new_img[d_y, a_x, :]) + L_a_x * (L_d_y * new_img[a_y, b_x, :] + L_a_y * new_img[d_y, b_x, :]), 0)
 			
 
-	plt.subplot(122),plt.imshow(img),plt.title('input')
-	plt.subplot(121),plt.imshow(enlarged_img),plt.title('output')
+	plt.subplot(121),plt.imshow(img),plt.title('input')
+	plt.subplot(122),plt.imshow(zoomed_img),plt.title('output')
 	plt.show()
 	
+	cv2.imwrite('zoomed_img',zoomed_img)
+	
 	return
-		
-img_interp(img, 10.0, 700,500)
+
+dir = os.path.dirname(__file__)
+filename = os.path.join(dir, 'rabbit.jpg')   
+
+img=cv2.imread(filename , -1)	
+img_zoom(img, 2.0, 200,200)  # parameters( image, scale, piv_x,piv_y)
